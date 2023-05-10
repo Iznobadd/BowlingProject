ceec86cb083d704fbcf6fe984866d586f1b104f9
Neograaph <max45270@gmail.com>
2023-05-11 01:13:24 +0200

    leaderboard access

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  @Output() leaderboardClicked = new EventEmitter<void>();
+
+  onLeaderboardClick() {
+    this.leaderboardClicked.emit();
+  }
+}

ecd8f21cf05d4bac183d815ac479834a659813d3
Neograaph <max45270@gmail.com>
2023-05-11 00:45:19 +0200

    palette de couleur + navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: {
+    content: ["./src/app/app.component.html", "./src/**/*.{html,ts}"],
+  },
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        primary: "#4C3D3D",
+        secondary: "#C07F00",
+        accent: "#FFD95A",
+        background: "#FFF7D4",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};

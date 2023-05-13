5d825cbbf62473c1ddc3a54b81aa5085e383f8f6
Neograaph <max45270@gmail.com>
2023-05-13 10:47:13 +0200

    pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { PageHomeComponent } from './page/page-home/page-home.component';
+import { PageRouletteComponent } from './page/page-roulette/page-roulette.component';
+import { PageLeaderboardComponent } from './page/page-leaderboard/page-leaderboard.component';
+import { PageContactComponent } from './page/page-contact/page-contact.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LeaderboardComponent],
+  declarations: [AppComponent, NavbarComponent, LeaderboardComponent, PageHomeComponent, PageRouletteComponent, PageLeaderboardComponent, PageContactComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/page/page-leaderboard/page-leaderboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-leaderboard',
+  templateUrl: './page-leaderboard.component.html',
+  styleUrls: ['./page-leaderboard.component.css']
+})
+export class PageLeaderboardComponent {
+
+}
